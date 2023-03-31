b75e6292636c0748df5321d27fc02e5d4f278b94
Danil <danil.komyshev777@gmail.com>
2023-04-01 00:20:07 +0400

    [ADDED] models

--- service/models.py
+++ service/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float, Null
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float, Null, DateTime
 
 from .database import Base
 
@@ -7,10 +7,69 @@ class AtimexOptions(Base):
     __tablename__ = "atimex_options"
 
     atimex_options_pk = Column(Integer, primary_key=True, index=True)
-    investor_id = Column(Integer, ForeignKey("investor.investor_id"))
+    investor_id = Column(Integer, ForeignKey("investor.investor_pk"), nullable=False)
 
-    deal_plus = Column(Float, index=True)
-    deal_minus = Column(Float, index=True)
-    waiting_time = Column(Float, index=True)
-    ask_investor = Column(String)
-    return_cost = Column(Boolean, default=Null)
+    deal_plus = Column(Float, index=True, default=Null)
+    deal_minus = Column(Float, index=True, default=Null)
+    waiting_time = Column(Float, index=True, default=Null)
+    ask_investor = Column(String, index=True, default=Null)
+    return_cost = Column(Boolean, index=True, default=Null)
+    multiplier = Column(String,  index=True, default=Null)
+    multiplier_value = Column(Float, index=True, default=Null)
+    change_multiplier = Column(Boolean, index=True, default=Null)
+    stop_loss = Column(String, index=True, default=Null)
+    stop_value = Column(Float, index=True, default=Null)
+    open_deals = Column(String, index=True, default=Null)
+    off_initiator = Column(String, index=True, default=Null)
+    switch_off = Column(Boolean, index=True, default=Null)
+    close_opened_deals = Column(String, index=True, default=Null)
+    notification = Column(Boolean, index=True, default=Null)
+    blacklist = Column(Boolean, index=True, default=Null)
+    accompany_transactions = Column(Boolean, index=True, default=Null)
+    exchange_connect = Column(Boolean, index=True, default=Null)
+    apikey_expired = Column(Boolean, index=True, default=Null)
+    closed_deals_myself = Column(Boolean, index=True, default=Null)
+    reconnected = Column(Boolean, index=True, default=Null)
+    recavery_model = Column(Boolean, index=True, default=Null)
+    buyhold_model = Column(Boolean, index=True, default=Null)
+    not_enough_margin = Column(String, index=True, default=Null)
+    accounts_in_different_currencies = Column(String, index=True, default=Null)
+    synchronize_deals = Column(Boolean, index=True, default=Null)
+    deals_not_opened = Column(Boolean, index=True, default=Null)
+    investor_closed_deals = Column(Boolean, index=True, default=Null)
+
+
+class Leader(Base):
+    __tablename__ = "leader"
+
+    leader_pk = Column(Integer, primary_key=True, index=True)
+    login = Column(String, index=True, nullable=False)
+    password = Column(String, index=True, nullable=False)
+    server = Column(String, index=True, nullable=False)
+    balance = Column(String, index=True, nullable=False)
+
+
+class Investor(Base):
+    __tablename__ = "investor"
+
+    investor_pk = Column(Integer, primary_key=True, index=True)
+    leader_id = Column(Integer, ForeignKey("leader.leader_pk"), nullable=False)
+    login = Column(String, index=True, nullable=False)
+    password = Column(String, index=True, nullable=False)
+    server = Column(String, index=True, nullable=False)
+    balance = Column(Float, index=True, nullable=False)
+
+
+class Position(Base):
+    __tablename__ = "position"
+
+    position_pk = Column(Integer, primary_key=True, index=True)
+    leader_id = Column(Integer, ForeignKey("leader.leader_pk"), nullable=False)
+
+    ticket = Column(Integer, index=True, nullable=False, unique=True)
+    time = Column(DateTime, index=True, nullable=False)
+    type = Column(String, index=True, nullable=False)
+    volume = Column(Float, index=True, nullable=False)
+    sell_price = Column(Float, index=True, nullable=False)
+    buy_price = Column(Float, index=True, nullable=False)
+    profit = Column(Float, index=True, nullable=False)
