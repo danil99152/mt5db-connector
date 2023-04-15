30238cccd56befbf03395046e5ea51b9b589300d
danil99152 <danil.komyshev777@gmail.com>
2023-04-15 06:10:57 +0400

    --[UPD] settings hos

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
 
     APP_TITLE: constr(min_length=1, max_length=255) = 'MT5Connector'
     APP_VERSION: constr(min_length=1, max_length=15) = '1'
-    APP_HOST: constr(min_length=1, max_length=15) = str(IPv4Address('127.0.0.1' if DEBUG else '0.0.0.0'))
+    APP_HOST: constr(min_length=1, max_length=15) = str(IPv4Address('0.0.0.0'))
     APP_PORT: conint(ge=0) = 8000
     APP_PATH: constr(min_length=1, max_length=255) = str(pathlib.Path(__file__).parent.resolve())
 
