be7ce5f5a28e305eed8fbb4740a3f55eed579a28
Danil <danil.komyshev777@gmail.com>
2023-05-17 07:55:09 +0300

    some fixes in settings.py

--- settings.py
+++ settings.py
@@ -7,8 +7,8 @@ from pydantic.validators import IPv4Address
 class Settings(BaseSettings):
     APP_TITLE: constr(min_length=1, max_length=255) = 'MT5Connector'
     APP_VERSION: constr(min_length=1, max_length=15) = '1'
-    APP_HOST: constr(min_length=1, max_length=15) = str(IPv4Address('127.0.0.1'))
-    APP_PORT: conint(ge=0) = 5000
+    APP_HOST: constr(min_length=1, max_length=15) = str(IPv4Address('0.0.0.0'))
+    APP_PORT: conint(ge=0) = 8000
     APP_PATH: constr(min_length=1, max_length=255) = str(pathlib.Path(__file__).parent.resolve())
 
     address = 'https://my.atimex.io:8000'
