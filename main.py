518368f8f87b5a9e0b3796d2699d9be355deb537
Danil <danil.komyshev777@gmail.com>
2023-07-11 05:18:25 +0300

    [UPD] requirements.txt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import logging
-import threading
 
 import uvicorn
 from fastapi import FastAPI
--- settings.py
+++ settings.py
@@ -1,13 +1,11 @@
 import pathlib
 
-from pydantic import BaseSettings, conint, constr
-from pydantic.validators import IPv4Address
+from pydantic import conint, constr
 
-
-class Settings(BaseSettings):
+class Settings:
     APP_TITLE: constr(min_length=1, max_length=255) = 'MT5Connector'
     APP_VERSION: constr(min_length=1, max_length=15) = '1'
-    APP_HOST: constr(min_length=1, max_length=15) = str(IPv4Address('0.0.0.0'))
+    APP_HOST: constr(min_length=1, max_length=15) = str('0.0.0.0')
     APP_PORT: conint(ge=0) = 8000
     APP_PATH: constr(min_length=1, max_length=255) = str(pathlib.Path(__file__).parent.resolve())
 
